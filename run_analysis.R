32d30fc2ff542954e35d44d3dcc1bc5bd7a6590a
emiliogozo <emilio.gozo@obf.ateneo.edu>
2016-02-16 14:54:52 +0800

    add run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,38 @@
+library(data.table)
+
+# function to read and process harusd
+read.harusd <- function(dataDir, colNames){
+	dataType <- basename(dataDir)
+	print(paste("Getting", dataType, "data"))
+	subjId <- fread(paste0(dataDir,"/subject_",dataType,".txt"))$V1
+	y_data <- fread(paste0(dataDir,"/y_",dataType,".txt"))$V1
+	x_data <- fread(paste0(dataDir,"/X_",dataType,".txt"))
+	names(x_data) <- colNames
+	return (data.table(subjid=subjId,activityid=y_data,x_data))
+}
+
+dataDir <- "UCIHARDataset"
+
+# read column names
+featNames <- fread(paste0(dataDir,"/features.txt"))$V2
+
+# read and process the data
+testDat <- read.harusd(paste(dataDir,"test",sep="/"),featNames)
+trainDat <- read.harusd(paste(dataDir,"train",sep="/"),featNames)
+
+# Merges the training and the test sets to create one data set
+mergedDat <- rbind(testDat, trainDat)
+
+# Extracts only the measurements on the mean and standard deviation for each measurement
+outDat <- mergedDat[,c(1,2,grep("mean|std", colnames(mergedDat))),with=F]
+
+# Uses descriptive activity names to name the activities in the data set
+activityNames <- fread(paste0(dataDir,"/activity_labels.txt"))
+names(activityNames) <- c("activityid","activityname")
+outDat <- activityNames[outDat,on="activityid"]
+
+# Appropriately labels the data set with descriptive variable names.
+names(outDat) <- tolower(gsub("\\.$","",gsub("[^a-zA-Z0-9]+","\\.",names(outDat))))
+
+# Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+summaryDat <- outDat[, lapply(.SD, mean), by=list(activityid,activityname,subjid)]
